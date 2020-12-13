061017f67430842bfeff21b6d69bd63fb0fbb3df
Dmytro Shvets <shvetsdmytro90@ukr.net>
2020-12-13 15:06:13 +0200

    finished

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { data, Images } from './data';
+import { Service } from '../services';
 
 @Component({
   selector: 'app-gallery',
@@ -8,11 +9,20 @@ import { data, Images } from './data';
 })
 export class GalleryComponent implements OnInit {
   public title = 'Gallery';
-  public images: Images = data;
+  public images: Images[] = data;
+  public selctedImage: Images;
+  
+  @ViewChild('plate')
+  public plate: ElementRef;
 
-  constructor() { }
+  constructor(private service: Service) {}
 
   ngOnInit(): void {
+    this.service.selectedImage.subscribe(selctedImage => this.selctedImage = selctedImage);
+  }
+
+  selecting(item: Images): void {
+    this.selctedImage = item;
   }
 
 }
--- src/app/services.ts
+++ src/app/services.ts
@@ -1,18 +1,40 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { data } from './gallery/data';
+import { data, Images } from './gallery/data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Service {
 
-  public imageLink = new BehaviorSubject<string>(data[0].urls.regular);
+  public index: number = 0;
+
+  public imageLink = new BehaviorSubject<string>(data[this.index].urls.regular);
   currentLink = this.imageLink.asObservable();
 
-  constructor() { }
+  public currentImage = new BehaviorSubject<Images>(data[this.index]);
+  selectedImage = this.currentImage.asObservable();
 
-  changingLink(link: string) {
-    this.imageLink.next(link);
+  constructor() {}
+
+  changingLink(i: number) {
+    this.index = i;
+    this.imageLink.next(data[this.index].urls.regular);
+  }
+
+  nextImage() {
+    if (this.index < data.length - 1) {
+      this.index++;
+    } else this.index = 0;
+    this.imageLink.next(data[this.index].urls.regular);
+    this.currentImage.next(data[this.index]);
+  }
+
+  previousImage() {
+    if (this.index > 0) {
+      this.index--;
+    } else this.index = data.length - 1;
+    this.imageLink.next(data[this.index].urls.regular);
+    this.currentImage.next(data[this.index]);
   }
 }
